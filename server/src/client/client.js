a9a248fca74926c0de2fbd225a54c4a7e28cc9bf
Eric Constantinides <eric@ericconstantinides.com>
2018-01-22 17:29:52 -0800

    Add the redux store to the client

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -2,11 +2,25 @@
 import React from 'react'
 import { hydrate } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+// createStore creates the store.
+// applyMiddleware hooks up middleware to redux
+import { createStore, applyMiddleware } from 'redux'
+// thunk for the async action creators
+import thunk from 'redux-thunk'
+// Provider ties the store and the react app together
+// Provider is a React Component
+import { Provider } from 'react-redux'
 import Routes from './Routes'
 
+// create the store (reducers, initial state, middleware):
+const store = createStore(reducers, {}, applyMiddleware(thunk))
+
+// wrap the store with the Provider component
 hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
